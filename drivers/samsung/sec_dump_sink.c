CruelKernel/samsung-exynos9820

drivers/samsung/sec_dump_sink: fix printk format errors
Signed-off-by: Denis Efremov <efremov@linux.com>

drivers/samsung/sec_dump_sink.c
@@ -60,7 +60,7 @@ static void sec_free_rdx_bootdev(phys_addr_t paddr, u64 size)
 	unsigned long pfn_start, pfn_end, pfn_idx;
 	int ret;
 
-	pr_info("start (0x%p, 0x%llx)\n", paddr, size);
+	pr_info("start (0x%p, 0x%llx)\n", (void *)paddr, size);
 
 	if (!sec_rdx_bootdev_paddr) {
 		pr_err("reserved addr is null\n");
@@ -130,7 +130,7 @@ static ssize_t sec_rdx_bootdev_proc_write(struct file *file,
 		err = -ENODEV;
 	} else {
 		if (count > sec_rdx_bootdev_size) {
-			pr_err("size is wrong %llu > %llu\n", count, sec_rdx_bootdev_size);
+			pr_err("size is wrong %lu > %u\n", count, sec_rdx_bootdev_size);
 			err = -EINVAL;
 			goto out;
 		}

mydlo260/Galaxy-S20

drivers/samsung/sec_dump_sink.c
@@ -61,7 +61,7 @@ static void sec_free_rdx_bootdev(phys_addr_t paddr, u64 size)
 	unsigned long pfn_start, pfn_end, pfn_idx;
 	int ret;
 
-	pr_info("start (0x%llx, 0x%llx)\n", paddr, size);
+	pr_info("start (0x%llx, 0x%llx)\n", (void *)paddr, size);
 
 	if (!sec_rdx_bootdev_paddr) {
 		pr_err("reserved addr is null\n");